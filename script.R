1e72d1e41ee0ab6b5a0d15f6ccf2e0b304920ef5
SumedhSankhe <sumedh.sankhe@gmail.com>
2018-08-07 17:46:19 -0400

    change factors to characters

--- script.R
+++ script.R
@@ -16,7 +16,7 @@ readTransform <- function(file){
   cat(" Reading",f,"....")
   
   if (grepl(".csv",file)){
-    data <- data.table(read.csv(file = file))
+    data <- data.table(read.csv(file = file, stringsAsFactors = F))
   }
   
   if(grepl(".xlsx",file)){
@@ -47,29 +47,45 @@ readTransform <- function(file){
   if(length(grep("NAIC_CODE",colnames(data)) >= 1))
     setnames(data,"NAIC_CODE","NAICS_CODE")
   
-  data <- data[,.(CASE_NUMBER, CASE_STATUS, CASE_SUBMITTED, DECISION_DATE, VISA_CLASS, EMPLOYMENT_START_DATE,
+  dataT <- data[,.(CASE_NUMBER, CASE_STATUS, CASE_SUBMITTED, DECISION_DATE, VISA_CLASS, EMPLOYMENT_START_DATE,
                   EMPLOYMENT_END_DATE, EMPLOYER_NAME, EMPLOYER_ADDRESS, EMPLOYER_CITY, EMPLOYER_STATE,
                   EMPLOYER_POSTAL_CODE, JOB_TITLE, SOC_CODE, SOC_NAME, NAICS_CODE, FULL_TIME_POSITION,
                   PREVAILING_WAGE, PW_UNIT_OF_PAY, PW_WAGE_SOURCE, WORKSITE_CITY, WORKSITE_STATE)]
   
-  data[, year := gsub(".csv","",paste0(substr(Sys.Date(), 1, 2), gsub("_.*", "", gsub(".*FY", "", file))))]
-  data <- data[EMPLOYER_STATE %in% state.abb]
+  dataT[,':='(PW_UNIT_OF_PAY = ifelse(PW_UNIT_OF_PAY == "",gsub("", NA_real_, PW_UNIT_OF_PAY),
+                                      PW_UNIT_OF_PAY),
+              PREVAILING_WAGE = ifelse(PREVAILING_WAGE == "", gsub("", NA_real_, PREVAILING_WAGE),
+                                       PREVAILING_WAGE))]
+  
+  unitPay <- data[CASE_NUMBER %in% dataT[is.na(PW_UNIT_OF_PAY),CASE_NUMBER]]
+  cat("\n",nrow(unitPay),"\n")
+  if(nrow(unitPay) != 0 & "WAGE_UNIT_OF_PAY" %in% colnames(unitPay)){
+    dataP <- merge(dataT[is.na(PW_UNIT_OF_PAY)], unitPay[,.(CASE_NUMBER,WAGE_UNIT_OF_PAY)])
+    dataP[,PW_UNIT_OF_PAY := WAGE_UNIT_OF_PAY]
+    dataP[,WAGE_UNIT_OF_PAY := NULL]
+    dataT <- rbind(dataT[!is.na(PW_UNIT_OF_PAY)], dataP)
+  }
+  
+  dataT[, ':='(year = gsub(".csv","",paste0(substr(Sys.Date(), 1, 2), gsub("_.*", "", gsub(".*FY", "", file)))),
+               PREVAILING_WAGE = as.numeric(PREVAILING_WAGE))]
+  
+  dataT <- dataT[EMPLOYER_STATE %in% state.abb]
   
   cat(" Standardizing pay \n")
-  data[,PREVAILING_WAGE := ifelse(PW_UNIT_OF_PAY == "Year", PREVAILING_WAGE,
+  dataT[,PREVAILING_WAGE := ifelse(PW_UNIT_OF_PAY == "Year", PREVAILING_WAGE,
                                   ifelse(PW_UNIT_OF_PAY == "Hour", 2080 * PREVAILING_WAGE,
                                          ifelse(PW_UNIT_OF_PAY == "Week", 52 * PREVAILING_WAGE,
                                                 ifelse(PW_UNIT_OF_PAY == "Month", 12 * PREVAILING_WAGE,
                                                        26 * PREVAILING_WAGE))))]
   
   cat(" Standardizing dates \n")
-  data[,':='(CASE_SUBMITTED = as.Date(CASE_SUBMITTED, "%m/%d/%Y"),
+  dataT[,':='(CASE_SUBMITTED = as.Date(CASE_SUBMITTED, "%m/%d/%Y"),
              DECISION_DATE = as.Date(DECISION_DATE, "%m/%d/%Y"),
              EMPLOYMENT_START_DATE = as.Date(EMPLOYMENT_START_DATE, "%m/%d/%Y"),
              EMPLOYMENT_END_DATE = as.Date(EMPLOYMENT_END_DATE, "%m/%d/%Y"))]
   
   cat(" Completed",f,"in",difftime(Sys.time(),st, units = "secs"),"seconds","\n\n\n")
-  return(data)
+  return(dataT)
 }
 
 
